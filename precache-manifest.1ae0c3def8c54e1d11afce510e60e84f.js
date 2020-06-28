21dd843ee783b9ea6769e7b09a6d905681ff4803
BarckCode <barckcode@gmail.com>
2020-06-28 15:46:07 +0200

    Updates

---
+++ precache-manifest.1ae0c3def8c54e1d11afce510e60e84f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9594acb80ca3f1d97f4f40bdc88891",
+    "url": "/Rick-Morty-SPA/index.html"
+  },
+  {
+    "revision": "4cd1a778d90b96eaceff",
+    "url": "/Rick-Morty-SPA/static/css/main.356952ec.chunk.css"
+  },
+  {
+    "revision": "2da83753549bf6fde073",
+    "url": "/Rick-Morty-SPA/static/js/2.8d4e9f4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rick-Morty-SPA/static/js/2.8d4e9f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd1a778d90b96eaceff",
+    "url": "/Rick-Morty-SPA/static/js/main.6a77051d.chunk.js"
+  },
+  {
+    "revision": "d38d2c81844d62cceaaa",
+    "url": "/Rick-Morty-SPA/static/js/runtime-main.0898524f.js"
+  },
+  {
+    "revision": "328ef7fcad0879883461e69fa4bf033c",
+    "url": "/Rick-Morty-SPA/static/media/arrow.328ef7fc.svg"
+  },
+  {
+    "revision": "782c905d432717470b71135dc0a7c8f4",
+    "url": "/Rick-Morty-SPA/static/media/home.782c905d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Rick-Morty-SPA/precache-manifest.1ae0c3def8c54e1d11afce510e60e84f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rick-Morty-SPA/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
